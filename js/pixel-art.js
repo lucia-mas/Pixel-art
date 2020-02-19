401dcc50a59e919ab88b0cf8c47e1f67876f0291
lucia <lucia@lucias-MacBook-Pro.local>
2020-02-18 23:39:28 -0300

    part 1 2 3 √

--- js/pixel-art.js
+++ js/pixel-art.js
@@ -24,12 +24,134 @@ var nombreColores = ['White', 'LightYellow',
 // Es decir, el que se elige con la rueda de color.
 var colorPersonalizado = document.getElementById('color-personalizado');
 
-colorPersonalizado.addEventListener('change', 
-  (function() {
+
+
+// Armado de grilla y de paleta colores
+
+var paleta = document.getElementById('paleta')
+var grilla = document.getElementById('grilla-pixeles')
+
+
+function recorrerColores (){
+  for (var i=0 ; i<nombreColores.length; i++){
+    var nvoDiv = document.createElement('div')
+    nvoDiv.className = 'color-paleta';
+    nvoDiv.style.backgroundColor = nombreColores[i];
+    paleta.appendChild(nvoDiv);
+  }
+}
+
+recorrerColores();
+
+function armadoGrilla (){
+  for (var i=0 ; i<1750; i++){
+    var nvoDivDos = document.createElement('div');
+    nvoDivDos.className = 'pixelGrilla';
+    nvoDivDos.style.backgroundColor = 'light-grey';
+   grilla.appendChild(nvoDivDos);
+  }
+}
+
+armadoGrilla();
+
+// Control de que jQuery está cargado: 
+// $(document).ready(function() {
+//   alert("jQuery esta funcionando !!");
+// });
+
+
+// Selección de color   https://www.w3schools.com/jquery/jquery_css.asp
+$(document).ready(function(){
+  $(".color-paleta").click(function(){
+    var $colorSeleccionado = $(this).css("background-color");
+    var $indicadorDeColor= $('#indicador-de-color');
+    $indicadorDeColor.css("background-color", $colorSeleccionado);
+  });
+});
+
+//Pintado de grilla
+$(document).ready(function(){
+   $('.pixelGrilla').click(function(){
+      var $indicadorDeColor= $('#indicador-de-color').css('background-color');
+     $(this).css("background-color", $indicadorDeColor);
+  })
+
+})
+
+//Color personalizado
+colorPersonalizado.addEventListener('change', (function () {
     // Se guarda el color de la rueda en colorActual
     colorActual = colorPersonalizado.value;
     // Completar para que cambie el indicador-de-color al colorActual
-
-
+    var indicadorDeColor = document.getElementById('indicador-de-color');
+    indicadorDeColor.style.backgroundColor = colorActual;
   })
 );
+
+
+//Pintar en movimiento. https://stackoverflow.com/questions/7700066/check-if-mousedown-with-an-if-statement
+
+var mouseDown=false;
+
+$(document).ready(function(){
+  $('.pixelGrilla').mousedown(function(){
+    mouseDown=true;
+    console.log('mousedown');
+  }).mouseup(function(){
+    mouseDown=false;
+    console.log('mouseUp');
+  });
+});
+
+
+  $(document).ready(function(){
+    $('.pixelGrilla').mouseover(function(){
+      if (mouseDown===true) { 
+        console.log('mouseover')
+      var $indicadorDeColor= $('#indicador-de-color').css('background-color');
+      $(this).css("background-color", $indicadorDeColor);
+      }
+    })
+  })
+
+//Borrar la pantalla   https://github.com/jquery/jquery-color     https://api.jquery.com/animate/ 
+$(document).ready(function(){
+  $("#borrar").click(function(){
+   var $divsGrilla =  $("#grilla-pixeles").find("div.pixelGrilla");
+   $($divsGrilla).animate({backgroundColor:'white'},1000);
+  });
+});
+
+
+//Cargar superhéroe
+$(document).ready(function(){
+  $("#batman").click(function(){
+    cargarSuperheroe(batman);
+  });
+});
+
+$(document).ready(function(){
+  $("#wonder").click(function(){
+    cargarSuperheroe(wonder);
+  });
+});
+
+$(document).ready(function(){
+  $("#flash").click(function(){
+    cargarSuperheroe(flash);
+  });
+});
+
+$(document).ready(function(){
+  $("#invisible").click(function(){
+    cargarSuperheroe(invisible);
+  });
+});
+
+
+//Guardar en png
+$(document).ready(function(){
+  $('#guardar').click(function(){
+    guardarPixelArt();
+  })
+})
